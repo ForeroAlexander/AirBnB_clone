188513a640f50a4fd0948ffe10b27908dbc950df
lepc1972 <githublepc@gmail.com>
2021-06-25 18:21:20 +0000

    do_create

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import cmd
 import shlex
 import sys
 import models
+import json
 from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
@@ -19,13 +20,30 @@ from models.user import User
 class HBNBcommand(cmd.Cmd):
       """ HBNB CLASS """
       prompt = '(hbnb)'
-      def do_EOF(self, argument):
+      def do_EOF(self, args):
             """ End Of Line"""
             print()
             return True
-      def do_quit(self, argument):
+      def do_quit(self, args):
             """ Close cmd """
             return True
+      def do_create(self, args):
+            '''
+            Create a new instance of class BaseModel and saves it
+            to the JSON file.
+        '''
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        try:
+            args = shlex.split(args)
+            new_instance = eval(args[0])()
+            new_instance.save()
+            print(new_instance.id)
+
+        except:
+            print("** class doesn't exist **")
+
 if __name__ == "__main__":
       """Infinte loop"""
       HBNBcommand().cmdloop()
