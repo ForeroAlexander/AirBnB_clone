be5bd8cb4f4b0ac3df4860ad78a60b093a36b2e7
lepc1972 <githublepc@gmail.com>
2021-06-26 09:35:44 -0500

    inits .py fixes

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,18 @@ create a unique FileStorage istance
 for you application
 """
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
 
 Storage = FileStorage()
 Storage.reload()
