431a289f243f7c890ce715a0f64fecdf795625d3
lepc1972 <githublepc@gmail.com>
2021-06-26 13:40:52 -0500

    init py changes

--- models/__init__.py
+++ models/__init__.py
@@ -4,8 +4,7 @@
 
 from models.engine.file_storage import FileStorage
 
-classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
-           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
+
 storage = FileStorage()
 storage.reload()
 
