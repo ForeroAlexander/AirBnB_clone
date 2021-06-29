9668d7a4283ace86eaa49f524336fc48ebdfdd4f
ForeroAlexander <foreroalex26@gmail.com>
2021-06-29 17:01:51 -0500

    place class

--- models/place.py
+++ models/place.py
@@ -14,6 +14,6 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = 0
-    longitude = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = ""  # amenity.ids format
