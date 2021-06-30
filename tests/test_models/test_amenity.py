47022837a1378afce844417c629623dc407633f7
lepc1972 <githublepc@gmail.com>
2021-06-30 10:29:28 -0500

    amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
-"""test"""
+
+'''
+    All the test for the amenity model are implemented here.
+'''
+
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
 
 
-class TestAmenity(unittest, TestCase):
-    """test"""
-    pass
+class TestAmenity(unittest.TestCase):
+    '''
+        Test Amenity class
+    '''
+
+    def test_Amenity_inheritence(self):
+        '''
+            testing  Amenity class Inherits from BaseModel
+        '''
+        new_amenity = Amenity()
+        self.assertIsInstance(new_amenity, BaseModel)
+
+    def test_Amenity_attributes(self):
+        '''
+            Testing Amenity class had name attribute.
+        '''
+        new_amenity = Amenity()
+        self.assertTrue("name" in new_amenity.__dir__())
+
+    def test_Amenity_attribute_type(self):
+        '''
+            Testing Amenity class had name attribute's type.
+        '''
+        new_amenity = Amenity()
+        name_value = getattr(new_amenity, "name")
+        self.assertIsInstance(name_value, str)
