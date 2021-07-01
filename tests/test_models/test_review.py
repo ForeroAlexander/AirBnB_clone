70da3ff0f869067430886637be30d2ad64a71185
lepc1972 <githublepc@gmail.com>
2021-06-30 22:10:37 -0500

    review test n permissions

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
-"""Test"""
+
+'''
+    All the test for the user model are implemented here.
+'''
+
+import unittest
+from models.base_model import BaseModel
+from models.review import Review
 
 
-class TestReview(unittest, TestCase):
-    """test Review"""
-    pass
+class TestReview(unittest.TestCase):
+    '''
+        Testing Review class
+    '''
+
+    def test_Review_inheritance(self):
+        '''
+            tests Review class BaseModel
+        '''
+        new_review = Review()
+        self.assertIsInstance(new_review, BaseModel)
+
+    def test_Review_attributes(self):
+        '''
+            Test Review class has place id, user id text
+            attributes.
+        '''
+        new_review = Review()
+        self.assertTrue("place_id" in new_review.__dir__())
+        self.assertTrue("user_id" in new_review.__dir__())
+        self.assertTrue("text" in new_review.__dir__())
+        place_id = getattr(new_review, "place_id")
+        user_id = getattr(new_review, "user_id")
+        text = getattr(new_review, "text")
+        self.assertIsInstance(place_id, str)
+        self.assertIsInstance(user_id, str)
+        self.assertIsInstance(text, str)
